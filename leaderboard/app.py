6963ea42ee6765ed6b5a71b5b34ccd91523e919c
Austin Wood <bonfyre@gmail.com>
2013-09-07 20:30:54 +1000

    Added counter to leaderboard app to test heroku

--- leaderboard/app.py
+++ leaderboard/app.py
@@ -3,9 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+c = 1
+
 @app.route('/')
 def hello():
-    return 'Hello Worlds!'
+	global c
+	c += 1
+	x = 'Hello %s' % c
+	return x
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
