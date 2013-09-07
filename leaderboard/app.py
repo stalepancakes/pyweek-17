06e8d053e5c788fb3dbbc029aa0d03b4bdae927f
Austin Wood <bonfyre@gmail.com>
2013-09-07 20:35:58 +1000

    Added DB boilerplate

--- leaderboard/app.py
+++ leaderboard/app.py
@@ -1,7 +1,11 @@
 import os
 from flask import Flask
+from flask.ext.heroku import Heroku
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+heroku = Heroku(app)
+db = SQLAlchemy(app)
 
 c = 1
 
